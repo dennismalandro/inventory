d0aaadd1265e861e45bcb7cb15e7dacee9adba95
Dennis Malandro <dennismalandro@gmail.com>
2018-05-08 09:50:09 -0400

    Add lead_times() function
    Change demand-rate function name from orders_to_rates() to demand_rates()
    
    Remove as.Date() conversion

--- R/util.R
+++ R/util.R
@@ -1,25 +1,43 @@
-# Convert dmd epoch/size tbl to dmd-rate tbl
-#' Calculate the nonparametric density of a numeric vector.
+#' Calculate demand rates from demand epochs and quantities
 #'
-#' @param .data tibble containing dates and quantities
-#' @param date order-date column
-#' @param qty order-quantity column
-#' @param ... grouping variable(s) (eg item)
-#' @return a demand-rate transformed tibble of (eg, demand) rates
+#' @param .data A tibble containing (at least) the following dates and quantities
+#' @param item item id
+#' @param date Epoch at which an order is placed
+#' @param qty Quantity orded
+#' @param dr_name Returned demand-rate variable name
+#' @return A tibble of demand rates by item
 #' @export
-orders_to_rates <- function(.data, date, qty, ...) {
+demand_rates <- function(.data, item, date, qty) {
+  item <- enexpr(item)
   date <- enexpr(date)
   qty <- enexpr(qty)
+  item_name <- quo_name(item)
   qty_name <- quo_name(qty)
-  grp_vars <- exprs(..., .named = TRUE)
+  # grp_vars <- exprs(..., .named = TRUE)
 
- .data %>%
-    mutate_if(is.POSIXt, as.Date) %>%
-    group_by(!!!grp_vars, !!date) %>%
+  .data %>%
+    group_by(!!item, !!date) %>%
     summarize(!!qty_name := sum(!!qty)) %>%
-    arrange(!!!grp_vars, !!date) %>%
+    arrange(!!item, !!date) %>%
     mutate(
-     `Interorder Duration` = interval(lag(!!date), !!date) %>%
+      'Interorder Duration' = interval(lag(!!date), !!date) %>%
         as.numeric('days'),
-     `Demand Rate` = !!qty / `Interorder Duration`) %>%
+      'Demand Rate' = !!qty / `Interorder Duration`) %>%
     drop_na()}
+
+
+#' Calculate lead times from work-order dates and completed dates
+#'
+#' @param .data A tibble containing (at least) dates for work order placement and completion
+#' @param item Item id
+#' @param lt_name Returned lead-time variable name
+#' @return .data with lead-time column appended
+#' @export
+lead_times <- function(.data, start, end) {
+  start <- enexpr(start)
+  end <- enexpr(end)
+
+  .data %>%
+    mutate(
+      'Lead Time' = interval(!!start, !!end) %>%
+        as.numeric('days'))}
