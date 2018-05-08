59ac100b34893235f13bacef51ed537b38f16ab6
Dennis Malandro <dennismalandro@gmail.com>
2018-05-08 18:00:21 -0400

    Simplify demand_rates()
    Remove item/grouping argument, now demand_rates() depends on external grouping/nesting

--- R/util.R
+++ R/util.R
@@ -1,36 +1,32 @@
-#' Calculate demand rates from demand epochs and quantities
+#' Compute demand rates from demand epochs and quantities
 #'
 #' @param .data A tibble containing (at least) the following dates and quantities
-#' @param item item id
 #' @param date Epoch at which an order is placed
 #' @param qty Quantity orded
 #' @return A tibble of demand rates by item
 #' @export
-demand_rates <- function(.data, item, date, qty) {
-  item <- enexpr(item)
+demand_rates <- function(.data, date, qty) {
   date <- enexpr(date)
   qty <- enexpr(qty)
-  item_name <- quo_name(item)
-  qty_name <- quo_name(qty)
-  # grp_vars <- exprs(..., .named = TRUE)
+  qty_name <- expr_name(qty)
 
-  .data %>%
-    group_by(!!item, !!date) %>%
+ .data %>%
+    group_by(!!date) %>%
     summarize(!!qty_name := sum(!!qty)) %>%
-    arrange(!!item, !!date) %>%
+    arrange(!!date) %>%
     mutate(
-      'Interorder Duration' = interval(lag(!!date), !!date) %>%
-        as.numeric('days'),
-      'Demand Rate' = !!qty / `Interorder Duration`) %>%
-    drop_na()}
+     'Interorder Duration' =
+        interval(lag(!!date), !!date) %>%
+          as.numeric('days'),
+     'Demand Rate' = !!qty / `Interorder Duration`) %>%
+   drop_na()}
 
 
-#' Calculate lead times from work-order dates and completed dates
+#' Compute lead times from work-order dates and completed dates
 #'
 #' @param .data A tibble containing (at least) dates for work order placement and completion
 #' @param start Date, start of lead time
 #' @param end Date, end of lead time
-#' @param item Item id
 #' @return .data with lead-time column appended
 #' @export
 lead_times <- function(.data, start, end) {
