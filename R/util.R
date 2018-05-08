7226ab18de6b133ce45c4774e555edd2360a2488
Dennis Malandro <dennismalandro@gmail.com>
2018-05-08 09:53:39 -0400

    Update function argument documentation

--- R/util.R
+++ R/util.R
@@ -4,7 +4,6 @@
 #' @param item item id
 #' @param date Epoch at which an order is placed
 #' @param qty Quantity orded
-#' @param dr_name Returned demand-rate variable name
 #' @return A tibble of demand rates by item
 #' @export
 demand_rates <- function(.data, item, date, qty) {
@@ -29,8 +28,9 @@ demand_rates <- function(.data, item, date, qty) {
 #' Calculate lead times from work-order dates and completed dates
 #'
 #' @param .data A tibble containing (at least) dates for work order placement and completion
+#' @param start Date, start of lead time
+#' @param end Date, end of lead time
 #' @param item Item id
-#' @param lt_name Returned lead-time variable name
 #' @return .data with lead-time column appended
 #' @export
 lead_times <- function(.data, start, end) {
